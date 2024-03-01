4a24a560967ab8d65f9e03ec66d8694cb9090c03
Thiago Vildosa <tvildosa@gmail.com>
2024-03-01 20:06:48 -0300

    desafio 1 completo

--- Lógica de programación/Funciones y listas/Desafio 1/app.js
+++ Lógica de programación/Funciones y listas/Desafio 1/app.js
@@ -5,9 +5,19 @@ function mensajeEnConsola() {
   console.log('El botón fue clicado');
 }
 
+function mensajeEnAlert() {
+  alert('Yo amo JS');
+}
+
 function mensajeEnPrompt() {
   let ciudad = prompt('Ingrese una ciudad de Brasil');
   alert(`Estuve en ${ciudad} y me acordé de ti`);
 }
 
+function suma() {
+  let num1 = prompt('Ingrese el primer número a sumar: ');
+  let num2 = prompt('Ingrese el segundo número a sumar: ');
+  let resultado = parseInt(num1) + parseInt(num2);
+  alert(`El resultado de la suma es: ${resultado}`);
+}
 
