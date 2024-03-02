699e9d12d76f113aebf41bf582a9959fc30becb1
Thiago Vildosa <tvildosa@gmail.com>
2024-03-02 01:39:15 -0300

    update

--- Lógica de programación/Funciones y listas/Ejercicio principal/app.js
+++ Lógica de programación/Funciones y listas/Ejercicio principal/app.js
@@ -1,9 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
-
-function intentoDeUsuario() {
-  alert('Click desde el boton');
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
 }
+
+function verificarIntento() {
+  let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+  console.log(typeof numeroUsuario);
+  console.log(numeroUsuario);
+  console.log(typeof numeroSecreto);
+  console.log(numeroSecreto);
+  console.log(numeroUsuario == numeroSecreto);
+  return;
+}
+
+function generarNumeroSecreto() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
